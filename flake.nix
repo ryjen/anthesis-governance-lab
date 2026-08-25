a771f9286c0320eb655de574b370092f77a91925
RJ <rybertjen@gmail.com>
2026-08-25 16:28:40 -0700

    chore(nix): add Governance Lab development shell
    * chore(nix): add governance lab dev shell
    
    * fix(nix): use coreutils for realpath and sha256sum

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Anthesis Governance Lab development shell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            bash
+            coreutils
+            cosign
+            curl
+            git
+            gnugrep
+            gnutar
+            gzip
+            jq
+          ];
+
+          shellHook = ''
+            echo "Anthesis Governance Lab dev shell"
+            echo "Includes cosign and documented validation dependencies."
+          '';
+        };
+      });
+}
